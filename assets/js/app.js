814f661659207d097c097666dd9ba69d7974c07a
abdur <abdur.cit.bd@gmail.com>
2024-06-08 13:51:11 +0600

    test

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+let crossBtn = document.querySelector('.cross-btn');
+let searchBtn = document.querySelector('.search_btn');
+let searchBox = document.querySelector('.search-box');
+let inputTest = document.querySelector('.input-test');
+
+searchBox.addEventListener('click',function(e){
+    e.preventDefault();
+    if(e.target.classList.contains('active_search')){
+        searchBox.classList.remove('active_search');
+    }
+});
+
+searchBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    searchBox.classList.add('active_search');
+    if(!(e.target.classList.contains('active_search'))){
+        inputTest.classList.add('active_input');
+    }
+});
+
+crossBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    searchBox.classList.remove('active_search');
+    
+});
